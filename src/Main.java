4cf9e2f50287ba13b76e088bce64b9842702f529
Александр <79691189430@yandex.ru>
2023-08-01 20:19:19 +0300

    home work is done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+        Author norton = new Author("Эндрю","Нортон");
+        Author nagosky = new Author("Эмили", "Нагоски");
+
+        Book vugoranie = new Book("Выгорание", nagosky, 2022);
+        Book sargassuInSpace = new Book("Саргассы в космосе", norton, 1992);
+
+        System.out.println(vugoranie.getTitle() +" "+ vugoranie.getAutor().getFirstName() +" "+ vugoranie.getAutor().getLastName()+" "+ vugoranie.getPublishingYear());
+
+        System.out.println(sargassuInSpace.getTitle() +" "+ sargassuInSpace.getAutor().getFirstName() +" "+ sargassuInSpace.getAutor().getLastName()+" "+ sargassuInSpace.getPublishingYear());
+        
+
     }
 }
